a687c30248b573caba1f39959387f2468a0a78f7
lazor254 <159952670+lazor254@users.noreply.github.com>
2024-05-28 17:17:10 +0300

    Update User_Input.py

--- User_Input.py
+++ User_Input.py
@@ -2,3 +2,13 @@ name = input("what's your name?: ")
 age = input("Enter your age: ")
 location = input("Where do you live?: ")
 print(f"Hello {name}, you are {age} years old and you live in {location}. in need of some help?")
+
+
+
+#output
+PS C:\Users\lazor\Documents\python module> python .\User_Input.py
+what's your name?: Lazor
+Enter your age: 24
+Where do you live?: Kitale
+Hello Lazor, you are 24 years old and you live in Kitale. in need of some help?
+PS C:\Users\lazor\Documents\python module> 
